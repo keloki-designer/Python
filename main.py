5c02a2e22c94de77112797da76172132408c5ae4
keloki <keloki@yandex.ru>
2024-02-03 16:02:57 +0400

    новая фича с принтом

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-# This is a sample Python script.
+def add(x, y):
+    return x + y
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def subtract(x, y):
+    return x - y
 
+def multiply(x, y):
+    return x * y
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("фича для гитхаба")
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Ошибка: деление на ноль"
 
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+choice = input("Введите номер операции (1/2/3/4): ")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Неверный ввод")
